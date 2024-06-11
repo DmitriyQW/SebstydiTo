afa4a58174f4d43e7966846cd2131e60a965a40f
DmitriyQW <varavkadmitrij@gmail.com>
2024-06-11 15:47:03 +0300

    ver usecase 4.0

--- shared/src/commonMain/kotlin/com/sebstydi/sebstydi/presentation/resume/state/ResumeState.kt
+++ shared/src/commonMain/kotlin/com/sebstydi/sebstydi/presentation/resume/state/ResumeState.kt
@@ -1,14 +1,21 @@
 package com.sebstydi.sebstydi.presentation.resume.state
 
 import LastName
+import com.sebstydi.sebstydi.domain.models.resume.values.Direction
+import com.sebstydi.sebstydi.domain.models.resume.values.Education
 import com.sebstydi.sebstydi.domain.models.resume.values.FirstName
 import com.sebstydi.sebstydi.domain.models.resume.values.MidleName
 import com.sebstydi.sebstydi.domain.models.resume.values.PhoneNumber
+import com.sebstydi.sebstydi.domain.models.resume.values.Skill
 
 data class ResumeState(
     val firstNameError: FirstName? = null,
     val lastNameError: LastName? = null,
     val midleNameError: MidleName? = null,
     val phoneNumberError: PhoneNumber? = null,
-    val isResumSend : Boolean = false
+    val educationError: Education? = null,
+    val direction: Direction? = null,
+    val isAddSkill : Boolean = false,
+    val isDeleteSkill : Boolean = false,
+    val isResumeSend : Boolean = false
 )
--- shared/src/commonMain/kotlin/com/sebstydi/sebstydi/ui/theme/Color.kt
+++ shared/src/commonMain/kotlin/com/sebstydi/sebstydi/ui/theme/Color.kt
@@ -2,70 +2,69 @@ package com.sebstydi.sebstydi.ui.theme
 
 
 
+const val GreenPrimaryLight = 0xff006e26
+const val OnGreenLight = 0xffffffff
+const val GreenContainerLight = 0xff6cff82
+const val OnGreenContainerLight = 0xff002106
 
-//val GreenPrimaryLight = Color(0xff006e26)
-//val OnGreenLight = Color(0xffffffff)
-//val GreenContainerLight = Color(0xff6cff82)
-//val OnGreenContainerLight = Color(0xff002106)
-//
-//val GreenSecondaryLight = Color(0xff526350)
-//val OnGreenSecondaryLight = OnGreenLight
-//val GreenSecondaryContainerLight = Color(0xffd4e8d0)
-//val OnGreenSecondaryContainerLight = Color(0xff101f10)
-//
-//val GreenTertiaryLight = Color(0xff39656b)
-//val OnGreenTertiaryLight = OnGreenLight
-//val GreenTertiaryContainerLight = Color(0xffbcebf2)
-//val OnGreenTertiaryContainerLight = Color(0xff001f23)
-//
-//val ErrorLight = Color(0xffba1a1a)
-//val OnErrorLight = Color(0xffffffff)
-//val ErrorContainerLight = Color(0xffffdad6)
-//val OnErrorContainerLight = Color(0xff410002)
-//
-//val BackgroundLight = Color(0xfffcfdf7)
-//val OnBackgroundLight = Color(0xff1a1c19)
-//val SurfaceLight = BackgroundLight
-//val OnSurfaceLight = OnBackgroundLight
-//val SurfaceVariantLight = Color(0xffdee5d9)
-//val OnSurfaceVariantLight = Color(0xff424940)
-//
-//val OutlineLight = Color(0xff72796f)
-//
-//// DARK
-//val GreenPrimaryDark = Color(0xff00e559)
-//val OnGreenDark = Color(0xff003910)
-//val GreenContainerDark = Color(0xff00531b)
-//val OnGreenContainerDark = Color(0xff6cff82)
-//
-//val GreenSecondaryDark = Color(0xffb9ccb5)
-//val OnGreenSecondaryDark = OnGreenDark
-//val GreenSecondaryContainerDark = Color(0xff3a4b39)
-//val OnGreenSecondaryContainerDark = Color(0xffd4e8d0)
-//
-//val GreenTertiaryDark = Color(0xffa1ced5)
-//val OnGreenTertiaryDark = Color(0xff00363c)
-//val GreenTertiaryContainerDark = Color(0xff1f4d53)
-//val OnGreenTertiaryContainerDark = Color(0xffbcebf2)
-//
-//val ErrorDark = Color(0xffffb4ab)
-//val OnErrorDark = Color(0xff690005)
-//val ErrorContainerDark = Color(0xff93000a)
-//val OnErrorContainerDark = Color(0xffffdad6)
-//
-//val BackgroundDark = Color(0xff1a1c19)
-//val OnBackgroundDark = Color(0xffe2e3dd)
-//val SurfaceDark = BackgroundDark
-//val OnSurfaceDark = OnBackgroundDark
-//val SurfaceVariantDark = Color(0xff424940)
-//val OnSurfaceVariantDark = Color(0xffc2c9bd)
-//
-//val OutlineDark = Color(0xff72796f)
-//
-//const val Purple80 = 0xFFD0BCFF
-//const val PurpleGrey80 = 0xFFCCC2DC
-//const val Pink80 = 0xFFEFB8C8
-//
-//const val Purple40 = 0xFF6650a4
-//const val PurpleGrey40 = 0xFF625b71
-//const val Pink40 = 0xFF7D5260
+const val GreenSecondaryLight = 0xff526350
+const val OnGreenSecondaryLight = OnGreenLight
+const val GreenSecondaryContainerLight = 0xffd4e8d0
+const val OnGreenSecondaryContainerLight = 0xff101f10
+
+const val GreenTertiaryLight = 0xff39656b
+const val OnGreenTertiaryLight = OnGreenLight
+const val GreenTertiaryContainerLight = 0xffbcebf2
+const val OnGreenTertiaryContainerLight = 0xff001f23
+
+const val ErrorLight = 0xffba1a1a
+const val OnErrorLight = 0xffffffff
+const val ErrorContainerLight = 0xffffdad6
+const val OnErrorContainerLight = 0xff410002
+
+const val BackgroundLight = 0xfffcfdf7
+const val OnBackgroundLight = 0xff1a1c19
+const val SurfaceLight = BackgroundLight
+const val OnSurfaceLight = OnBackgroundLight
+const val SurfaceVariantLight = 0xffdee5d9
+const val OnSurfaceVariantLight = 0xff424940
+
+const val OutlineLight = 0xff72796f
+
+// DARK
+const val GreenPrimaryDark = 0xff00e559
+const val OnGreenDark = 0xff003910
+const val GreenContainerDark = 0xff00531b
+const val OnGreenContainerDark = 0xff6cff82
+
+const val GreenSecondaryDark = 0xffb9ccb5
+const val OnGreenSecondaryDark = OnGreenDark
+const val GreenSecondaryContainerDark = 0xff3a4b39
+const val OnGreenSecondaryContainerDark = 0xffd4e8d0
+
+const val GreenTertiaryDark = 0xffa1ced5
+const val OnGreenTertiaryDark = 0xff00363c
+const val GreenTertiaryContainerDark = 0xff1f4d53
+const val OnGreenTertiaryContainerDark = 0xffbcebf2
+
+const val ErrorDark = 0xffffb4ab
+const val OnErrorDark = 0xff690005
+const val ErrorContainerDark = 0xff93000a
+const val OnErrorContainerDark = 0xffffdad6
+
+const val BackgroundDark = 0xff1a1c19
+const val OnBackgroundDark = 0xffe2e3dd
+const val SurfaceDark = BackgroundDark
+const val OnSurfaceDark = OnBackgroundDark
+const val SurfaceVariantDark = 0xff424940
+const val OnSurfaceVariantDark = 0xffc2c9bd
+
+const val OutlineDark = 0xff72796f
+
+const val Purple80 = 0xFFD0BCFF
+const val PurpleGrey80 = 0xFFCCC2DC
+const val Pink80 = 0xFFEFB8C8
+
+const val Purple40 = 0xFF6650a4
+const val PurpleGrey40 = 0xFF625b71
+const val Pink40 = 0xFF7D5260
