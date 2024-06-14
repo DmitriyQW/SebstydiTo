3e451d2b8bb02a33e02ec47d687ddc3bcd0d36e0
sonyafis <sonyafisun@mail.ru>
2024-06-14 15:01:00 +0300

    SharedRes

--- androidApp/src/main/java/com/sebstydi/sebstydi/android/presentation/mainActivity/MainActivity.kt
+++ androidApp/src/main/java/com/sebstydi/sebstydi/android/presentation/mainActivity/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : ComponentActivity() {
             SebstydiTheme {
                 val navController = rememberNavController()
                 NavGraph(navController)
-
             }
         }
     }
